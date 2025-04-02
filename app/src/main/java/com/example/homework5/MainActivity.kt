17763b405d5d86c9d8338979931ca15f51a54987
michellebello <michellebelloveramendi@gmail.com>
2025-04-02 12:59:24 -0400

    "finished giving the last styling details to make app look nice"

--- app/src/main/java/com/example/homework5/MainActivity.kt
+++ app/src/main/java/com/example/homework5/MainActivity.kt
@@ -1,33 +1,61 @@
 package com.example.homework5
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.homework5.screens.AmphibianScreen
 import com.example.homework5.screens.AmphibianViewModel
 import com.example.homework5.ui.theme.Homework5Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "App started")
         setContent {
             Homework5Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    val amphibianViewModel: AmphibianViewModel =
-                        viewModel(factory = AmphibianViewModel.Factory)
-                    AmphibianScreen(
-                        amphibianUIState = amphibianViewModel.amphibianUIState,
-                        afterAction = amphibianViewModel::getAmphibian
-                    )
-                }
+                val appBackgroundColor = Color(0XFFf2f2eb)
+                Scaffold (
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar (
+                            title = {
+                                Text("Amphibians", fontWeight = FontWeight.W400)
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(containerColor = appBackgroundColor)
+                        )
+                    }
+                ) { innerPadding ->
+                    Surface (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = appBackgroundColor
+                    ){
+                            val amphibianViewModel: AmphibianViewModel =
+                                viewModel(factory = AmphibianViewModel.Factory)
+                            AmphibianScreen(
+                                amphibianUIState = amphibianViewModel.amphibianUIState,
+                                afterAction = amphibianViewModel::getAmphibian
+                            )
+                        }
+                    }
+
             }
         }
     }
--- app/src/main/java/com/example/homework5/screens/AmphibianScreen.kt
+++ app/src/main/java/com/example/homework5/screens/AmphibianScreen.kt
@@ -2,19 +2,26 @@ package com.example.homework5.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.homework5.model.AmphibianInfo
@@ -60,9 +67,8 @@ fun ErrorScreen(afterAction: () -> Unit){
 @Composable
 fun AmphibianTotalList(listOfAmphibians: List<AmphibianInfo>){
     LazyColumn(
-        modifier = Modifier
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
     ){
         items(listOfAmphibians) { amphibian ->
             AmphibianCard(amphibian)
@@ -73,27 +79,41 @@ fun AmphibianTotalList(listOfAmphibians: List<AmphibianInfo>){
 @Composable
 fun AmphibianCard(amphibian: AmphibianInfo){
     Card(
+        colors = CardDefaults.cardColors(containerColor = Color(0XFFc1c9c1)),
         modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)
+            .fillMaxWidth()
+            .padding(start=15.dp, end=15.dp)
     ){
         Column(
+            modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Text(
-                text = amphibian.name
+                text = amphibian.name + " (" + amphibian.type +")",
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Start,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(10.dp)
+                    .fillMaxWidth()
             )
             AsyncImage(
+                modifier = Modifier.fillMaxWidth(),
                 contentDescription = amphibian.name,
+                contentScale = ContentScale.Crop,
                 model = ImageRequest.Builder(context = LocalContext.current)
                     .data(amphibian.imageSrc)
                     .crossfade(true)
-                    .build(),
-                modifier = Modifier.fillMaxWidth()
+                    .build()
             )
             Text (
-                text = amphibian.description
+                text = amphibian.description,
+                fontWeight = FontWeight.W300,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+
             )
         }
     }
+    Spacer(modifier = Modifier.size(25.dp))
 }
